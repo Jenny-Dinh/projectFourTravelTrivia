84698b1b902087f5e7aefe42343e087da2c01c82
Cheriele Mordido <eyelmordido@gmail.com>
2020-06-11 20:19:20 -0400

    start button functional, working on free pass

--- triviajs/app.js
+++ triviajs/app.js
@@ -1,29 +1,65 @@
-console.log('Hello');
-
 const travelTrivia = {};
 
-travelTrivia.url = 'https://opentdb.com/api.php?amount=10&category=22&difficulty=hard&type=multiple';
+travelTrivia.url = 'https://opentdb.com/api.php';
 
-
-travelTrivia.getQuestions = function (query) {
-    $.ajax({
+travelTrivia.getData = function() {
+    return $.ajax({
         url: travelTrivia.url,
         method: 'GET',
         dataType: 'json',
         data: {
-            format: 'json',
-            q: query
+            amount: 1,
+            category: 22,
+            difficulty: 'hard',
+            type: 'multiple'
         }
-    }).then(function(results){
-        console.log(results)
-    })
+     })
+};
+
+// store promise in a variable
+travelTrivia.Data = travelTrivia.getData()
+
+travelTrivia.Data.then( function (arrayOfData) {
+    
+    arrayOfObj = arrayOfData.results[0];
+
+    travelTrivia.displayQuestion(arrayOfObj);
+
+    travelTrivia.freePass();
+
+    
+})
+// display question on the page  
+travelTrivia.displayQuestion = function(array) {
+    const question = array.question;
+    $('.question').html(`
+    <p>${question}</p>`);
+}
+// scroll to main window when start button is clicked
+travelTrivia.startTrivia = function() {
+    $('#startBtn').on('click', function() {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $('main').offset().top
+        }, 500);
+    });
 }
 
+// free pass button on click displays a different question and set of answers and disables
+travelTrivia.freePass = function() {
+    $('#freePass').on('click', function() {
+        console.log('Free pass');
+        // $('#freePass').attr('disabled', 'true');
+        travelTrivia.getData();
+        travelTrivia.displayQuestion(arrayOfObj);
+    })
+}
+        
 //initalizating 
-travelTrivia.init = function () {
-    travelTrivia.getQuestions();
+travelTrivia.init = function() {
+    travelTrivia.getData();
+    travelTrivia.startTrivia();
 }
 
 $(document).ready(function(){
-    
- })
+    travelTrivia.init();    
+})
